485d25a8f36a78c10b290fc0cbfd624a1a3b43b3
javierduarte69420 <javierduarte69420@hotmail.com>
2026-01-29 20:56:00 -0600

    Delete proyecto_netlify_backend directory

--- proyecto_netlify_backend/src/components/Divider.tsx
+++
@@ -1,5 +0,0 @@
-export const Divider = () => {
-  return (
-    <div className="bg-yellow-400 box-border caret-transparent h-1.5"></div>
-  );
-};
--- proyecto_netlify_backend/src/sections/Footer/components/FooterContent.tsx
+++
@@ -1,9 +0,0 @@
-import { FooterGrid } from "@/sections/Footer/components/FooterGrid";
-
-export const FooterContent = () => {
-  return (
-    <div className="box-border caret-transparent max-w-[1140px] break-words w-full mx-auto px-3 sm:px-4 md:px-6 py-6 sm:py-8">
-      <FooterGrid />
-    </div>
-  );
-};
